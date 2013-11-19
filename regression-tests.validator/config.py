6a84b97c6e3b20151187a447bf930bfaaba6d014
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2013-11-19 11:03:45 +0100

    add local testbed, inspired by the sidnlabs workbench. after config.py, run with "named -c named.conf -g -p 5300"

---
+++ regression-tests.validator/config.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+from subprocess import check_output, check_call
+from os import chdir, mkdir
+from shutil import rmtree
+
+# BASE = 'bad-dnssec.example.net'
+BASE = ''
+DEPTH = 4
+FLAVORS = [
+    'ok',
+    # 'bogussig',
+    'nods',
+    # 'sigexpired',
+    # 'signotincepted',
+    # 'unknownalgorithm'
+]
+
+def genzone(name, flavor, depth, conffile):
+    if depth > DEPTH:
+        return
+
+    if flavor:
+        if name:
+            myname = '%s.%s' % (flavor, name)
+        else:
+            myname = flavor
+    else:
+        myname = name
+
+    f = open('zones/%s.zone' % (myname or 'ROOT'), 'w')
+    print myname or 'ROOT'
+    print "generating [%s] at depth %s" % (myname, depth)
+
+    f.write("""
+$ORIGIN .
+%(myname)s   86400   IN SOA ns1.example.com. hostmaster.example.com. 1 3600 3600 3600 3600
+%(myname)s   86400   IN NS  %(myname)s
+%(myname)s   86400   IN A   127.0.0.127
+""" % dict(myname=myname or '.'))
+
+    for next in FLAVORS:
+        dsset = genzone(myname, next, depth+1, conffile)
+        if dsset:
+            if next != 'nods':
+                f.write('\n'.join(dsset)+'\n')
+
+    f.close()
+
+    chdir("zones")
+    ksk = check_output(['ldns-keygen', '-a', 'RSASHA256', '-k', myname or '.']).strip()
+    zsk = check_output(['ldns-keygen', '-a', 'RSASHA256', myname or '.']).strip()
+    check_call(['ldns-signzone', '%s.zone' % (myname or 'ROOT'), ksk, zsk])
+    chdir("..")
+
+    conffile.write("""
+zone "%(zone)s"{
+    type master;
+    file "./zones/%(fbase)s.zone.signed";
+};
+""" % dict(zone = myname or '.', fbase = myname or 'ROOT'))
+
+    check_call(['../pdns/pdnssec', '--config-dir=.', 'set-presigned', myname])
+
+    if myname == '':
+        with open('rootDS', 'w') as f:
+            f.write(open('zones/%s.ds' % ksk).read().strip().split('\t')[3])
+
+    return [open('zones/%s.ds' % ksk).read().strip()]
+
+if __name__ == '__main__':
+    try:
+        rmtree('zones')
+    except OSError:
+        pass
+    mkdir('zones')
+    with open('pdns.conf', 'w') as f:
+        f.write("""
+socket-dir=./
+launch=bind
+bind-dnssec-db=./dnssec.sqlite3
+bind-config=./named.conf
+""")
+    check_call(['../pdns/pdnssec', '--config-dir=.', 'create-bind-db', 'dnssec.sqlite3'])
+    conffile = open('named.conf', 'w')
+    genzone(BASE, '', 0, conffile)
+    conffile.close()
+
