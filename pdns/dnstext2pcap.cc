e0dc3177faec8d85242f7f326ede36dbdd5962dc
Peter van Dijk <peter.van.dijk@powerdns.com>
2017-05-17 14:15:26 +0200

    change to per line format

--- pdns/dnstext2pcap.cc
+++ pdns/dnstext2pcap.cc
@@ -22,7 +22,7 @@ void setaddress(struct in_addr *out, u_short *port, const string &value)
   string myval = value;
   u_short myport;
   std::replace( myval.begin(), myval.end(), '#', ':');
-  cout<<"   setaddress got ["<<myval<<"]"<<endl;
+  // cout<<"   setaddress got ["<<myval<<"]"<<endl;
   ComboAddress ca(myval);
   memcpy(out, &ca.sin4.sin_addr.s_addr, sizeof(ca.sin4.sin_addr.s_addr));
   myport = ca.sin4.sin_port;
@@ -36,13 +36,18 @@ void setflags(dnsheader *dh, string &flags)
   if(flags[0] != '(' || flags[flags.size()-1] != ')')
     throw runtime_error("invalid flags");
   flags = flags.substr(1, flags.size()-2);
-  cout<<"  got flags: ["<<flags<<"]"<<endl;
+  // cout<<"  got flags: ["<<flags<<"]"<<endl;
   boost::split(flagv, flags, boost::is_any_of(" "));
   for(auto &flag: flagv) {
-    cout<<"   got flag: "<<flag<<endl;
+    // cout<<"   got flag: "<<flag<<endl;
     flag = strip(flag);
-    if (flag == "RD") { cout<<"    setting RD"<<endl; dh->rd=1; }
-    else if(flag == "RA") { cout<<"    setting RA"<<endl; dh->ra=1; }
+    if (flag == "RD") {
+      // cout<<"    setting RD"<<endl;
+      dh->rd=1;
+    } else if(flag == "RA") {
+      // cout<<"    setting RA"<<endl;
+      dh->ra=1;
+    }
   }
 }
 
@@ -64,110 +69,110 @@ int main(int argc, char **argv)
 
   string token;
 
-  while(!infile.eof()) {
-    uint16_t ipid = 0;
-    infile >> token;
-    cout<<"got token '"<<token<<"'"<<endl;
-    if(token != "{") {
-        throw runtime_error("packet start missing");
-    }
+  struct pdns_pcap_pkthdr pheader;
+  struct udphdr uh;
+  struct ip ih;
+  DNSName qname;
+  QType qtype;
+  uint16_t qid;
+  string flags;
+  uint16_t reqsize;
 
-    struct pdns_pcap_pkthdr pheader;
-    struct udphdr uh;
-    struct ip ih;
-    DNSName qname;
-    QType qtype;
-    uint16_t qid;
-    string flags;
-    uint16_t reqsize;
+  uint16_t ipid = 0;
 
-    while(!infile.eof()) {
-      ih.ip_v=4;
-      ih.ip_hl = sizeof(ih) >> 2;
-      ih.ip_tos = 0;
-      // ih.ip_len
-      ih.ip_id=ipid++;
-      ih.ip_off=0;
-      ih.ip_ttl=127;
-      ih.ip_p=17;
-      ih.ip_sum=0;
+  for(std::string line ; std::getline(infile, line) ; ) {
+    ih.ip_v=4;
+    ih.ip_hl = sizeof(ih) >> 2;
+    ih.ip_tos = 0;
+    // ih.ip_len
+    ih.ip_id=ipid++;
+    ih.ip_off=0;
+    ih.ip_ttl=127;
+    ih.ip_p=17;
+    ih.ip_sum=0;
 
-      string to, value;
-      infile >> token;
-      if(token == "}") {
-        getline(infile, value); // discard
-        cout<<" writing packet with qname "<<qname.toString()<<endl;
-        vector<uint8_t> content;
-        DNSPacketWriter pw(content, qname, qtype.getCode(), 1, 0);
-        pw.getHeader()->id = htons(qid);
-        setflags(pw.getHeader(), flags);
-        pw.commit();
-        uh.uh_ulen = htons(sizeof(uh) + content.size());
-        uh.uh_sum = 0;
+    vector<string> parts;
 
-        ih.ip_len = htons(sizeof(ih) + sizeof(uh) + content.size());
+    // cout<<"got line '"<<line<<"'"<<endl;
+    boost:split(parts, line, boost::is_any_of(" "));
+    // cout<<"line has "<<parts.size()<<" parts"<<endl;
 
-        pheader.len = htonl(sizeof(ih) + sizeof(uh) + content.size());
-        pheader.caplen = pheader.len;
+    vector<string> tokens;
+    bool combine = false;
+    for(const auto& i : parts) {
+      // cout<<"part: "<<i<<endl;
 
-        cout<<"lengths pcap/ip/udp/dns/reqsize: "<<ntohl(pheader.len)<<", "
-                                                 <<ntohs(ih.ip_len)<<", "
-                                                 <<ntohs(uh.uh_ulen)<<", "
-                                                 <<content.size()<<", "
-                                                 <<reqsize<<endl;
-
-        if (content.size() != reqsize) throw runtime_error("content/reqsize mismatch");
-
-        fwrite(&pheader, 1, sizeof(pheader), out);
-        fwrite(&ih, 1, sizeof(ih), out);
-        fwrite(&uh, 1, sizeof(uh), out);
-        fwrite(content.data(), 1, content.size(), out);
-
-        goto next;
-      }
-      infile >> to;
-      getline(infile, value);
-      value = strip(value);
-      if (token == "timestamp") {
-        uint32_t ts;
-        ts = std::stoul(value);
-        cout<<"   got timestamp "<<ts<<endl;
-        pheader.ts.tv_sec = htonl(ts);
-        pheader.ts.tv_usec = 0;
-      } else if (token == "start-time") {
-        double ts;
-        uint32_t sec, usec;
-        ts = std::stod(value);
-        sec = ts;
-        usec = 1.0e9 * (ts - sec);
-        cout<<"   got start-time "<<sec<<";"<<usec<<endl;
-        pheader.ts.tv_sec = htonl(sec);
-        pheader.ts.tv_usec = htonl(usec);
-      } else if (token == "client-address") {
-        setaddress(&ih.ip_src, &uh.uh_sport, value);
-      } else if (token == "local-address") {
-        setaddress(&ih.ip_dst, &uh.uh_dport, value);
-      } else if (token == "name") {
-        qname = DNSName(value);
-        cout<<"   got qname "<<value<<" parsed as "<<qname.toString()<<endl;
-      } else if (token == "query-type") {
-        qtype = value;
-      } else if (token == "query-id") {
-        qid = std::stoul(value);
-      // FIXME: for rcode: can we fake up a response packet to make dnsreplay happy, even if it has no content?
-      // } else if (token == "result-code") {
-      //   if (value == "noerror") rcode = 0;
-      //   else throw runtime_error("unknown result-code");
-      } else if (token == "query-class") {
-        if (value != "IN") throw runtime_error("unknown query-class");
-      } else if (token == "flags") {
-        flags = value;
-      } else if (token == "request-size") {
-        reqsize = std::stoul(value);
+      if (!i.size()) {
+        tokens.push_back(i);
+        continue;
+      } else if (combine) {
+        tokens[tokens.size()-1] += " " + i;
+      } else if (!combine && (i.at(0) == '(' ||
+                              i.substr(0, 4) == "LVP(")) {
+        tokens.push_back(i);
+        combine = true;
       } else {
-        cout<<"  ignored token/to/value ["<<token<<"] ["<<to<<"] ["<<value<<"]"<<endl;
+        tokens.push_back(i);
       }
+
+      if (i.at(i.size()-1) == ')')
+        combine = false;
+
     }
-  next:;
+    for(const auto& i : tokens) {
+      // cout<<"token: "<<i<<endl;
+    }
+
+    // cout<<"line has "<<tokens.size()<<" tokens"<<endl;
+
+    uint32_t ts;
+    ts = std::stoul(tokens[0]);
+    // cout<<"   got timestamp "<<ts<<endl;
+    pheader.ts.tv_sec = htonl(ts);
+    pheader.ts.tv_usec = 0;
+
+    setaddress(&ih.ip_src, &uh.uh_sport, tokens[2]);
+    setaddress(&ih.ip_dst, &uh.uh_dport, tokens[3]);
+
+    qname = DNSName(tokens[4]);
+    // cout<<"   got qname "<<tokens[4]<<" parsed as "<<qname.toString()<<endl;
+
+    if (tokens[5] != "IN") {
+      cerr<<"unknown query-class"<<endl;
+      continue;
+    }
+
+    qtype = tokens[6];
+
+    qid = std::stoul(tokens[9]);
+
+    flags = tokens[10];
+
+    // cout<<" writing packet with qname "<<qname.toString()<<endl;
+    vector<uint8_t> content;
+    DNSPacketWriter pw(content, qname, qtype.getCode(), 1, 0);
+    pw.getHeader()->id = htons(qid);
+    setflags(pw.getHeader(), flags);
+    pw.commit();
+    uh.uh_ulen = htons(sizeof(uh) + content.size());
+    uh.uh_sum = 0;
+
+    ih.ip_len = htons(sizeof(ih) + sizeof(uh) + content.size());
+
+    pheader.len = htonl(sizeof(ih) + sizeof(uh) + content.size());
+    pheader.caplen = pheader.len;
+
+    // cout<<"lengths pcap/ip/udp/dns/reqsize: "<<ntohl(pheader.len)<<", "
+    //                                          <<ntohs(ih.ip_len)<<", "
+    //                                          <<ntohs(uh.uh_ulen)<<", "
+    //                                          <<content.size()<<", "
+    //                                          <<reqsize<<endl;
+
+    // if (content.size() != reqsize) throw runtime_error("content/reqsize mismatch");
+
+    fwrite(&pheader, 1, sizeof(pheader), out);
+    fwrite(&ih, 1, sizeof(ih), out);
+    fwrite(&uh, 1, sizeof(uh), out);
+    fwrite(content.data(), 1, content.size(), out);
   }
 }
