9dd73b85cfb795beb650c4efddf4596f843c101a
Peter van Dijk <peter.van.dijk@powerdns.com>
2019-04-02 17:34:32 +0200

    luawrapper: throw when somebody tries to make a 0-indexed array

--- ext/luawrapper/include/LuaContext.hpp
+++ ext/luawrapper/include/LuaContext.hpp
@@ -2137,6 +2137,34 @@ struct LuaContext::Pusher<std::unordered_map<TKey,TValue,THash,TKeyEqual>> {
     }
 };
 
+
+
+
+// vectors of int, X pairs
+template<typename TType2>
+struct LuaContext::Pusher<std::vector<std::pair<int,TType2>>> {
+    static const int minSize = 1;
+    static const int maxSize = 1;
+
+    static PushedObject push(lua_State* state, const std::vector<std::pair<int,TType2>>& value) noexcept {
+        static_assert(Pusher<typename std::decay<int>::type>::minSize == 1 && Pusher<typename std::decay<int>::type>::maxSize == 1, "Can't push multiple elements for a table key");
+        static_assert(Pusher<typename std::decay<TType2>::type>::minSize == 1 && Pusher<typename std::decay<TType2>::type>::maxSize == 1, "Can't push multiple elements for a table value");
+
+        auto obj = Pusher<EmptyArray_t>::push(state, EmptyArray);
+
+        if(value.size() && value.begin()->first == 0) {
+            throw std::runtime_error("0-indexed vector spotted");
+        }
+
+        for (auto i = value.begin(), e = value.end(); i != e; ++i)
+            setTable<TType2>(state, obj, i->first, i->second);
+        
+        return obj;
+    }
+};
+
+
+
 // vectors of pairs
 template<typename TType1, typename TType2>
 struct LuaContext::Pusher<std::vector<std::pair<TType1,TType2>>> {
