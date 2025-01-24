df7919ad6c6b2cc25e5c62871e6457f4de304e2e
Peter van Dijk <peter.van.dijk@powerdns.com>
2025-01-24 12:20:39 +0100

    work

--- modules/lmdbbackend/lmdbbackend.hh
+++ modules/lmdbbackend/lmdbbackend.hh
@@ -273,8 +273,7 @@ private:
                    index_on<TSIGKey, DNSName, &TSIGKey::name>>
     ttsig_t;
 
-  typedef TypedDBI<Netmask>
-    tnetworks_t;
+  MDBDbi d_tnetworks;
 
   int d_asyncFlag;
 
