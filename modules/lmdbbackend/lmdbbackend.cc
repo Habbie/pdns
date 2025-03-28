ccbf427b7547c2e0c77f7969d9a2aec02daee57d
Peter van Dijk <peter.van.dijk@powerdns.com>
2025-03-28 14:23:28 +0100

    lmdb viewList: dedup badly

--- modules/lmdbbackend/lmdbbackend.cc
+++ modules/lmdbbackend/lmdbbackend.cc
@@ -1321,6 +1321,8 @@ void LMDBBackend::viewList(vector<string>& result)
 {
   auto txn = d_tdomains->getEnv()->getROTransaction();
 
+  set<string> tmp;
+
   auto cursor = txn->getROCursor(d_tviews);
 
   MDBOutVal key{}; // <view, dnsname> - probably needs dnsname reversed like in domains_v5_0
@@ -1337,13 +1339,15 @@ void LMDBBackend::viewList(vector<string>& result)
       auto [view, zone] = splitField(key.getNoStripHeader<string>(), '\x0');
       auto tag = val.get<string>();
       cerr<<"in view ["<<view<<"], zone ["<<zone<<"] has tag ["<<tag<<"]"<<endl;
-      result.emplace_back(view);
+      tmp.insert(view);
     } catch (std::exception &e) {
       cerr<<e.what()<<": "<<makeHexDump(key.getNoStripHeader<string>())<<" / "<<makeHexDump(val.get<string>())<<endl;
     }
 
     ret = cursor.next(key, val); // this should use some lower bound thing to skip to the next view, also avoiding duplicates in `result`
   } while (ret != MDB_NOTFOUND);
+
+  copy(tmp.begin(), tmp.end(), std::back_inserter(result));
 }
 
 void LMDBBackend::viewListZones(const string& inview, vector<ZoneName>& result)
